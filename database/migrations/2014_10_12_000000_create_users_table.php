6d62f9ac81f06a9307727a2ddc503984638473c7
michowler <mlhy9778@hotmail.com>
2020-06-09 21:02:57 +0800

    added bootstrap template

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,10 +15,24 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id');
+            $table->string('google_id');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('description')->nullable();                        
+            $table->string('summary')->nullable();                                    
+            $table->string('age')->nullable();                        
+            $table->string('country')->nullable();           
+            $table->string('gender')->nullable();            
+            $table->string('education_level')->nullable();
+            $table->string('latest_edu')->nullable();   
+            $table->string('occupation')->nullable();       
+            $table->string('working_level')->nullable();
+            $table->string('latest_work_pos')->nullable();          
+            $table->string('birth_date')->nullable();            
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
+            $table->string('contact_num')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
